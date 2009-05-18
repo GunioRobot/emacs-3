f1fac89b5d043e21a26558f9400e43dc655638ca
cjb <casey.brant@gmail.com>
2009-05-18 17:46:54 -0500

    added two new shortcut keys -- scroll up/down one line at a time

--- keys-config.el
+++ keys-config.el
@@ -7,5 +7,22 @@
 (global-set-key [?\C-`] 'longlines-mode)
 (global-set-key [?\C-\]] 'other-window)
 (global-set-key [?\C-6] 'vi-mode)
+(global-set-key [?\M-p] 'down-one)
+(global-set-key [?\M-n] 'up-one)
+
+
+
+(defun up-one ()
+ "idea taken from Tom Laudeman"
+ (interactive)
+ (scroll-up 1))
+
+(defun down-one ()
+ "idea taken from Tom Laudeman"
+ (interactive)
+ (scroll-down 1))
+
+
+
 
 (provide 'keys-config)
