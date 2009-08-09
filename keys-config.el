75fe7afac2756546c33bae98b4d135f26123f42c
Casey <cjb@BLUE.(none)>
2009-08-09 14:10:13 -0500

    Added 'notes' file for things I want to remember. Added new variable for v23 in emacs-config

--- keys-config.el
+++ keys-config.el
@@ -4,7 +4,7 @@
 (global-set-key [?\C-w] 'backward-kill-word)
 (global-set-key [?\C-\M-w] 'kill-region)
 (global-set-key [?\M-'] 'clipboard-kill-region)
-(global-set-key [?\C-`] 'longlines-mode)
+(global-set-key [?\C-`] 'visual-line-mode)
 (global-set-key [?\C-\]] 'other-window)
 (global-set-key [?\C-6] 'vi-mode)
 (global-set-key [?\M-p] 'down-one)
