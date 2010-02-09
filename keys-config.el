9e862c01b6e897e50e02530f18c76d711b80cf41
Casey Brant <cjb@Faure.local>
2010-02-09 12:14:32 -0600

    Updated keys-config to fix a couple things

--- keys-config.el
+++ keys-config.el
@@ -1,11 +1,11 @@
 ;; cjb's custom keybindings
 
-(global-set-key "\C-xy" 'clipboard-yank)
+(global-set-key [?\C-x y] 'clipboard-yank)
 (global-set-key [?\C-w] 'backward-kill-word)
 (global-set-key [?\C-\M-w] 'kill-region)
 (global-set-key [?\M-'] 'clipboard-kill-region)
-(global-set-key [?\C-`] 'visual-line-mode)
-(global-set-key [?\C-\]] 'other-window)
+(global-set-key [?\C-`] 'longlines-mode)
+(global-set-key [?\C-]] 'other-window)
 (global-set-key [?\C-6] 'vi-mode)
 (global-set-key [?\M-p] 'down-one)
 (global-set-key [?\M-n] 'up-one)
