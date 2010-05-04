6502c3edce8590d671a72eaaed9c15b61ff187f9
BaseCase <casey.brant@gmail.com>
2010-05-03 20:17:40 -0500

    Fixed magit not knowing where git is at. Closes #16.

--- magit-config.el
+++ magit-config.el
@@ -1,6 +1,11 @@
 ;; magit is the coolest interface for git evar...right now this file does like nothing
+;; You don't really need all the other stuff that comes with magit. Just use magit.el
+;; latest version is 0.7
 
 
+; hopefully tell magit where git actually is
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/git/bin")) 
+(setq exec-path (append exec-path '("/usr/local/git/bin"))) 
 
 (require 'magit)
 
