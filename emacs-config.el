8cc402bfe673dfc8b5c0db0f51af8b36de4556dd
Casey Brant <casey.brant@gmail.com>
2011-08-27 09:43:30 -0500

    UTF-8 stuff, and now ansi-term works!

--- emacs-config.el
+++ emacs-config.el
@@ -75,5 +75,12 @@
     (if (not (string-match "/$" name))
         (rename-buffer (concat name "/") t))))
 
+;;unicode funtimes
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
 (provide 'emacs-config)
 
