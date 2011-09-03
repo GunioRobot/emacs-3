591a5ffeda3af33deaba370d326cd012b38028bf
Casey Brant <casey.brant@gmail.com>
2011-09-03 11:52:12 -0500

    'other-window bound to C-] in ansi-term windows.

--- term-config.el
+++ term-config.el
@@ -1,6 +1,7 @@
 (add-hook 'term-mode-hook
 	  (lambda ()
 	    (define-key term-raw-map (kbd "M-x") 'execute-extended-command)
+	    (define-key term-raw-map (kbd "C-]") 'other-window)
 	    (hl-line-mode -1)  
 	    ;;(define-key term-raw-map (kbd "C-y") 'term-paste)
 	    ;;(define-key term-raw-map (kbd "M-y") 'anything-show-kill-ring)
